3ad8096004998c64d502b4f1dd08d44c1778e850
teodoramateeva <123024883+teodoramateeva@users.noreply.github.com>
2024-12-28 11:13:25 +0200

    Update Feature_Select.py

--- Feature_Select.py
+++ Feature_Select.py
@@ -122,6 +122,6 @@ if __name__ == "__main__":
     X, Y = ExtractColumns(df)
     all_acc, MIF = SelectFeatures(X, Y)
     top_Features = GetTopFeatures(MIF, top_n=3, common_n=5)
-    feature_df = pd.DataFrame(top_Features, columns=["Feature"])
+    feature_df = pd.DataFrame(top_Features, columns=["Feature", "Importance"])
     feature_df.to_csv(args.output, index=False)
     print(f"Top features saved to {args.output}")
